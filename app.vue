5009028dda2cf2679051e3b93592d657c83dd8a7
bonomite <thomasbono.com@gmail.com>
2024-06-01 14:07:29 -0400

    initial scene

--- app.vue
+++ app.vue
@@ -1,5 +1,27 @@
+<script lang="ts" setup>
+import { TresCanvas } from "@tresjs/core"
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <TresCanvas window-size clear-color="#82DBC5">
+    <TresPerspectiveCamera :position="[3, 3, 3]" :look-at="[0, 0, 0]" :fov="45" />
+    <TresMesh>
+      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+      <TresMeshBasicMaterial color="orange" />
+    </TresMesh>
+    <TresAmbientLight :intensity="0.5" />
+  </TresCanvas>
 </template>
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+#app {
+  height: 100%;
+  width: 100%;
+}
+</style>
