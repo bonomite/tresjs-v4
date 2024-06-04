8b1dc5cb80d1a6cf22848a272af510132a3297b7
bonomite <thomasbono.com@gmail.com>
2024-06-04 11:32:45 -0400

    added a plain and a shadow

--- app.vue
+++ app.vue
@@ -1,14 +1,34 @@
 <script lang="ts" setup>
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from "three"
 import { TresCanvas } from "@tresjs/core"
+
+const gl = {
+  shadows: true,
+  alpha: true,
+  shadowMapType: BasicShadowMap,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: NoToneMapping,
+}
 </script>
 
 <template>
-  <TresCanvas window-size clear-color="#82DBC5">
+  <TresCanvas window-size v-bind="gl">
     <TresPerspectiveCamera :position="[3, 3, 3]" :look-at="[0, 0, 0]" :fov="45" />
-    <TresMesh>
+    <OrbitControls />
+    <TresMesh cast-shadow>
       <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
-      <TresMeshBasicMaterial color="orange" />
+      <MeshPhongMaterial color="orange" />
     </TresMesh>
+    <TresMesh
+      :position="[0, -2, 0]"
+      :rotation="[-1.5, 0, 0]"
+      :scale="[8, 8, 8]"
+      receive-shadow
+    >
+      <TresPlaneGeometry />
+      <MeshPhongMaterial color="#FFFFFF" />
+    </TresMesh>
+    <TresDirectionalLight :position="[0, 2, 4]" :intensity="1.2" cast-shadow />
     <TresAmbientLight :intensity="0.5" />
   </TresCanvas>
 </template>
@@ -19,6 +39,7 @@ body {
   padding: 0;
   height: 100%;
   width: 100%;
+  background-color: red;
 }
 #app {
   height: 100%;
